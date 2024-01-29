9554aa878186a8176564a13a17155c74bf10d6cb
Từ Quân <callmequantu@gmail.com>
2024-01-29 15:47:31 +0700

    change

--- src/components/componentB/ComponentB.jsx
+++ src/components/componentB/ComponentB.jsx
@@ -6,9 +6,9 @@ const ComponentB = ({ url }) => {
     return (
         <div className={styles.compB}>
             {url?.map((item, index) => (
-                <Link target="__blank" href={item} style={{ display: 'flex', gap: '10px' }}>
+                <Link key={item} target="__blank" href={item} style={{ display: 'flex', gap: '10px' }}>
                     <span>{index + 1}.</span>
-                    <span key={item}>{item}</span>
+                    <span>{item}</span>
                 </Link>
             ))}
         </div>
